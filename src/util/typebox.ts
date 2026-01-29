fc2465b9ca0dbe476b508a8c6ba59b84131f41ee
Tobias Strebitzer <tobias.strebitzer@magloft.com>
2026-01-30 07:25:34 +0800

    fix build

--- src/util/typebox.ts
+++
@@ -1,5 +0,0 @@
-import { Type, UnsafeOptions } from '@sinclair/typebox'
-
-export function stringEnum<T extends readonly string[]>(values: T, options: UnsafeOptions = {}) {
-  return Type.Unsafe<T[number]>({ type: 'string', enum: [...values], ...options })
-}
